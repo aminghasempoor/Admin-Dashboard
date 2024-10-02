6039b3ccd1c5535d3d5511020d630d7e9872d760
AminGhasempoor <aminghasempoor79@gmail.com>
2024-10-02 16:07:56 +0330

    event news component

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,17 +1,19 @@
 "use client"
 import {useState} from "react";
 import {Calendar} from "@/components/ui/calendar";
+import EventNews from "@/components/EventNews";
 
 const EventCalendar = () => {
     const [date, setDate] = useState<Date | undefined>(new Date())
     return(
-        <div>
+        <div className="rounded-md">
             <Calendar
                 mode="range"
                 selected={date}
                 onSelect={setDate}
                 className="rounded-md border flex justify-center items-center w-full"
             />
+            <EventNews />
         </div>
     )
 }
